2b76946d00d1f6417fd18ad2a234da7657475d72
Jacob Repp <jacobrepp@gmail.com>
2015-03-14 02:04:20 -0700

    cleanup samples

---
+++ mnames.py
@@ -0,0 +1,8 @@
+import hashlib
+
+hashlib.sha1("Deadwing LE").hexdigest()[:8]
+#'ba3397ab'
+hashlib.sha1("Overgrowth LE").hexdigest()[:8]
+#'db7f7d9c'
+hashlib.sha1("Catallena LE").hexdigest()[:8]
+#'7d8ca22e'
